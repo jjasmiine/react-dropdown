576aabe818a7f421addc8c65bf234f144716244b
lljasmine <84794871+lljasmine@users.noreply.github.com>
2021-11-17 15:03:51 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Consulting from './components/pages/Consulting';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import Contact from './components/pages/Contact';
+import SignUp from './components/pages/SignUp';
+import Marketing from './components/pages/Marketing';
+import Home from './components/pages/Home';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' exact component={Services} />
+        <Route path='/products' exact component={Products} />
+        <Route path='/contact' exact component={Contact} />
+        <Route path='/signUp' exact component={SignUp} />
+        <Route path='/marketing' exact component={Marketing} />
+        <Route path='/consulting' exact component={Consulting} />
+      </Routes>
+    </Router>
   );
 }
 
